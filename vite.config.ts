b86321ccbe185766f8f0eab4d4f647f91bdd2f47
zeyad2 <zeyadsattar.me@gmail.com>
2025-09-02 05:26:38 +0300

    fix: add react-globe.gl dependency for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+    rollupOptions: {
+      external: ['react-globe.gl']
+    }
+  },
   plugins: [react()],
   publicDir: 'src/public',
   optimizeDeps: {
@@ -11,4 +16,5 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
+  
 });
